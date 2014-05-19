b8a97e6df86a6470ecab56794960370488a046e8
s1078714 <s1078714@student.hsleiden.nl>
2014-05-19 17:51:02 +0200

    herschrijving helloworld
    inclusief makefile

--- Week 2/HelloWorld.cpp
+++ Week 2/HelloWorld.cpp
@@ -5,23 +5,15 @@
  *      Author: Gunnar
  */
 
-#include <HelloWorld.h>
 #include<avr/io.h>       // Header file for basic avr input/output
-     #include<util/delay.h>   // header file for delay generation
-     #define BV(x) (1<<x)     // See text below
 
-     int main(void)
-     {
-	DDRC=0xFF;      // PORTC declared as output
-	PORTC=0xFF;     // PORTC is initially high to off the led initially
-	while(1==1)     // infinite loop as 1 is always equals 1
+void main(void)
+{
+	DDRB = 0b00000001; // Signaal op PortB0 (is nu output)
+	PORTB = 0b00000001; // 5 Volt op PortB0
+
+	while (1)
 	{
-		PORTC=~(BV(5));     // led glow here, Making 5th bit of PORTC LOW
-		_delay_ms(1000);    // one second delay
-		PORTC=BV(5);        // led do not glow here
-		_delay_ms(1000);
+
 	}
-	return 0;
-
-    }
-
+}
