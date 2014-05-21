ad5a27265f600d3644d524bcb8348b48bed42acc
s1078714 <s1078714@student.hsleiden.nl>
2014-05-21 11:34:54 +0200

    switch geimplementeerd in HelloWorld

--- Week 2/HelloWorld/main.c
+++ Week 2/HelloWorld/main.c
@@ -10,12 +10,22 @@
 
 int main(void)
 {
-	DDRB |= 1 << PINB0; // Data Direction Register output for only
-						// pin 0 on port B
+	DDRB |= 1 << PINB0; 	// Data Direction Register output PINB0
+	DDRB &= ~(1 << PINB1); 	// Data Direction Register input PINB1
+	PORTB |= 1 << PINB1; 	// Set PINB1 to a high reading
 
 	while (1)
 	{
 		PORTB ^= 1 << PINB0;	// XOR (^) to toggle only pin 0 on Port B
-		_delay_ms(100);			// Delay between the toggling on and onff of 100 ms
+		_delay_ms(1000);		// Delay between the toggling on and off of 100 ms
+
+		if (bit_is_clear(PINB, 1))
+		{
+			_delay_ms(10);
+		}
+		else
+		{
+			_delay_ms(100);
+		}
 	}
 }
