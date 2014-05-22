55c58bef10f706b9bbf497eff6d58069121690b1
s1078714 <s1078714@student.hsleiden.nl>
2014-05-22 12:14:37 +0200

    teller telt tot 10, switcht tussen links en rechts!

--- Week 2/Teller2/main.c
+++ Week 2/Teller2/main.c
@@ -7,17 +7,72 @@
 
 #include <avr/io.h>
 #include <avr/delay.h>
-
+int x = 100;
 int main(void)
 {
 	DDRD = 0b11111111; // Signaal op PortD0 (is nu output)
 	//PORTD = 0b00000001; // 5 Volt op PortD0
+	DDRC = 0b0110000;
+
+
 
 	while (1)
 	{
-		PORTD = 0b00000010; // pd1 rechtsboven segment
-		_delay_ms(50);
-		PORTD = 0b00000011; // pd1 rechtsboven segment + pd0 bovenmidden segment
-		_delay_ms(50);
+		displayLinks();
+		displayRechts();
 	}
+
+
+}
+
+displayLinks()
+{
+	PORTC = 0b0100000;
+
+	PORTD = 0b00000110; // 1
+	_delay_ms(x);
+	PORTD = 0b11011011; // 2
+	_delay_ms(x);
+	PORTD = 0b01001111; // 3
+	_delay_ms(x);
+	PORTD = 0b01100110; // 4
+	_delay_ms(x);
+	PORTD = 0b01101101; // 5
+	_delay_ms(x);
+	PORTD = 0b01111101; // 6
+	_delay_ms(x);
+	PORTD = 0b00000111; // 7
+	_delay_ms(x);
+	PORTD = 0b11111111; // 8
+	_delay_ms(x);
+	PORTD = 0b11101111; // 9
+	_delay_ms(x);
+	PORTD = 0b00111111; // 0
+	_delay_ms(x);
+}
+
+displayRechts()
+{
+	PORTC = 0b0010000;
+
+	PORTD = 0b00000110; // 1
+	_delay_ms(x);
+	PORTD = 0b11011011; // 2
+	_delay_ms(x);
+	PORTD = 0b01001111; // 3
+	_delay_ms(x);
+	PORTD = 0b01100110; // 4
+	_delay_ms(x);
+	PORTD = 0b01101101; // 5
+	_delay_ms(x);
+	PORTD = 0b01111101; // 6
+	_delay_ms(x);
+	PORTD = 0b00000111; // 7
+	_delay_ms(x);
+	PORTD = 0b11111111; // 8
+	_delay_ms(x);
+	PORTD = 0b11101111; // 9
+	_delay_ms(x);
+	PORTD = 0b00111111; // 0
+	_delay_ms(x);
 }
